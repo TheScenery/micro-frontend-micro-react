9150d1ade5fc62ce97349f16facb7ab99986207a
TheScenery <liuwenbin.lwb@gmail.com>
2022-07-12 22:21:14 +0800

    to micro app

---
+++ config-overrides.js
@@ -0,0 +1,26 @@
+const { name } = require('./package');
+
+module.exports = {
+  webpack: function(config, env) {
+    config.output.library = `${name}-[name]`;
+    config.output.libraryTarget = 'umd';
+    config.output.chunkLoadingGlobal = `webpackJsonp_${name}`;
+    config.output.globalObject = 'window';
+
+    return config;
+  },
+  devServer: function(configFunction) {
+    return function(options) {
+      // Create the default config by calling configFunction with the proxy/allowedHost parameters
+      const config = configFunction(options);
+
+      config.headers = {
+        'Access-Control-Allow-Origin': '*',
+      };
+      config.historyApiFallback = true;
+      config.hot = false;
+      config.liveReload = false;
+      return config;
+    };
+  }
+};
